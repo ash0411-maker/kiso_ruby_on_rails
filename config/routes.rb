2cb92a81ea43703e9d775c11c3303f5ea46795de
ash <satoshi.0411@icloud.com>
2020-11-15 14:57:21 +0900

    [add] title_helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root "top#index"
+  get "/about" => "top#about", as: "about_path"
 
   1.upto(18) do |n|
     get "lesson/step#{n}(/:name)" => "lesson#step#{n}"
