380d87d5ee3451563e2114c4132b1bd782fb48d6
ash <satoshi.0411@icloud.com>
2020-11-15 09:19:47 +0900

    [add] redirection

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -2,4 +2,16 @@ class LessonController < ApplicationController
   def step1
     render plain: "コンチは、#{params[:name]}さん"
   end
+
+  def step2
+    render plain: "#{params[:controller]}" + "#" + "#{params[:action]}"
+  end
+
+  def step3
+    redirect_to action: "step4"
+  end
+
+  def step4
+    render plain: "step4に移動したで！！！"
+  end
 end
